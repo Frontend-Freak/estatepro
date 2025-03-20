84f351a514d9c4b7fe94d5df382432e5e93d8f4c
Wh04m7 <slipknot.rulit3@gmail.com>
2025-03-21 01:00:35 +0300

    Header

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
 import { defineConfig } from 'vite';
+import injectHTML from 'vite-plugin-html-inject';
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
     root: './src', //сборка из папки src
@@ -6,7 +9,28 @@ export default defineConfig({
         port: 3000, //(запуск на 3000 порту)
         open: true, //(настройка чтобы страница открывалась по умолчанию в браузере
     },
+    publicDir: './src/assets',
     build: {
-        outDir: '../dist', // сборка в корне проекта в папке dist
+        outDir: '../dist',
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: './src/index.html',
+                pricing: './src/pricing.html',
+                contact: './src/contact.html',
+            },
+        },
     },
+    plugins: [
+        injectHTML(),
+        ViteMinifyPlugin(),
+        ViteImageOptimizer({
+            png: {
+                quality: 80,
+            },
+            jpg: {
+                quality: 80,
+            },
+        }),
+    ],
 });
