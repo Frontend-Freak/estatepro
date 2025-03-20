a7f2ba74ac69de2c0a8a39d5435be690379b2c25
Wh04m7 <slipknot.rulit3@gmail.com>
2025-03-21 00:41:47 +0300

    added eslint, stylelint, prettier

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
-       export default defineConfig({
-        root: './src', //сборка из папки src
-       		server: {
-			port: 3000, //(запуск на 3000 порту)
-	open: true, //(настройка чтобы страница открывалась по умолчанию в браузере
-		},
-		build: {
-			outDir: '../dist', // сборка в корне проекта в папке dist
-		}
-       })
+export default defineConfig({
+    root: './src', //сборка из папки src
+    server: {
+        port: 3000, //(запуск на 3000 порту)
+        open: true, //(настройка чтобы страница открывалась по умолчанию в браузере
+    },
+    build: {
+        outDir: '../dist', // сборка в корне проекта в папке dist
+    },
+});
